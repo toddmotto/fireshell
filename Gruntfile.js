1f4fd3faa66d2ca9a20984a1753c0437f71256c8
yzlow <cephyz@gmail.com>
2014-02-02 22:51:41 +0800

    livereload mount dir uses project config
    Connect/livereload now uses the app value as specified in project config.

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
       livereload: {
         options: {
           middleware: function (connect) {
-            return [lrSnippet, mountFolder(connect, 'app')];
+            return [lrSnippet, mountFolder(connect, grunt.config('project.app'))];
           }
         }
       }
