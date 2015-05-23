5f89deab0dc6571e13b3f81a5b919711aefab6b2
Adam Brewer <adamcbrewer@gmail.com>
2015-05-23 14:15:48 +0100

    Sass sourcemaps working now

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -11,35 +11,29 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var config = require('../config.js');
 var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
 var minifycss = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
 var stylestats = require('gulp-stylestats');
 var handleErrors = require('../util/handle-errors');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', [], function () {
+gulp.task('sass', function () {
 
     var pipeline = sass(config.sass.src, {
             style: 'expanded',
             sourcemap: true
         })
         .on('error', handleErrors)
+
+        // autoprefix, minify and sourcemaps
+        .pipe(autoprefixer(config.sass.autoprefixer))
+        .pipe(minifycss())
         .pipe(sourcemaps.write())
 
-        // Un-prefixed
-        .pipe(rename({ basename: config.sass.basename + '.unprefixed' }))
-        .pipe(gulp.dest(config.sass.dest))
-
-        // Prefixed
-        .pipe(autoprefixer(config.sass.autoprefixer))
-        .pipe(rename({ basename: config.sass.basename }))
-        .pipe(gulp.dest(config.sass.dest))
-
-        // Minified
         .pipe(rename({ basename: config.sass.basename, suffix: '.min' }))
-        .pipe(minifycss())
         .pipe(gulp.dest(config.sass.dest))
 
+        // Style statistics
         .pipe(stylestats(config.stylestats))
         .pipe(rename({ basename: config.sass.basename + '.stats' }))
         .pipe(gulp.dest(config.sass.dest));
